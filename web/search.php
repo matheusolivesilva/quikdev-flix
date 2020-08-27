6700ba8bf3ec6a613106d01dc38ee9b93d59b068
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-08-27 00:44:28 -0300

    feat(web): create search form
    This form wil be responsible to search the movies by name.

---
+++ web/search.php
@@ -0,0 +1,8 @@
+<section id="search-movies">
+    <h1>QuickDevFlix</h1>
+    <h2>Come to the purple side of the force!</h2>
+    <form id="search">
+        <input type="text" name="search" placeholder="Search for a movie...">
+        <button type="submit"><i class="fas fa-search"></i> Search</button>
+    </form>
+</section>
