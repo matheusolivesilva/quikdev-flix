709b2dc7f893f1d557b318e61508ca04741aea07
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-08-28 12:55:29 -0300

    refactor(api): order movies by name

--- api/app/Http/Controllers/MovieController.php
+++ api/app/Http/Controllers/MovieController.php
@@ -31,26 +31,16 @@ class MovieController extends Controller
         
         $allTrendingMovies = [];
         foreach($foundTrendingMovies['results'] as $movie) {
-            // $singleMovieResponse = $this->doRequest($this->singleMovieUri . $movie['id']);
-            // $singleFoundMovie = json_decode($singleMovieResponse->getBody(), true);
-            // $allTrendingMovies['movies'][] = MovieFactory::create($singleFoundMovie);
             $allTrendingMovies['movies'][] = (object) $this->show($movie['id']);
         }
 
+        $allTrendingMovies['movies'] = $this->orderMoviesByAlphabetically($allTrendingMovies['movies']);
         return response() ->json(
             $allTrendingMovies,
             $trendingMoviesResponse->getStatusCode() 
         );
     }
-
-    private function doRequest($uri): Response
-    {
-        return $this->client->request(
-            'GET', 
-            $uri . self::apiKeyQueryParameter . self::apiKey
-        );
-    }
-
+    
    public function show(int $id)
    {
         $singleMovieResponse = $this->doRequest($this->singleMovieUri . $id);
@@ -62,5 +52,24 @@ class MovieController extends Controller
             $singleMovieResponse->getStatusCode()
         );
     }
+    
+    private function doRequest($uri): Response
+    {
+        return $this->client->request(
+            'GET', 
+            $uri . self::apiKeyQueryParameter . self::apiKey
+        );
+    }
 
+    private function orderMoviesByAlphabetically($allMovies)
+    {
+        usort($allMovies, function($currentMovie, $nextMovie) {
+            $currentMovieEncoded = json_encode($currentMovie, true);
+            $nextMovieEncoded = json_encode($nextMovie, true);
+            $currentMovieDecoded = json_decode($currentMovieEncoded);
+            $nextMovieDecoded = json_decode($nextMovieEncoded);
+            return strcmp($currentMovieDecoded->original->name, $nextMovieDecoded->original->name);
+        });
+        return $allMovies;
+    }
 }
